12cf3473e214535b1c2493cb40207eda4d152341
kenny6292 <kennymichael322@gmail.com>
2026-09-20 07:37:09 +0100

    Wire HavenStone property modules

--- wp-content/plugins/havenstone-core/havenstone-core.php
+++ wp-content/plugins/havenstone-core/havenstone-core.php
@@ -7,9 +7,10 @@
  * Requires PHP: 8.1
  */
 if (!defined('ABSPATH')) exit;
-
-define('HAVENSTONE_CORE_VERSION', '1.0.0');
-define('HAVENSTONE_CORE_PATH', plugin_dir_path(__FILE__));
-
-require_once HAVENSTONE_CORE_PATH . 'includes/post-types.php';
-require_once HAVENSTONE_CORE_PATH . 'includes/taxonomies.php';
+define('HAVENSTONE_CORE_VERSION','1.0.0');
+define('HAVENSTONE_CORE_PATH',plugin_dir_path(__FILE__));
+require_once HAVENSTONE_CORE_PATH.'includes/post-types.php';
+require_once HAVENSTONE_CORE_PATH.'includes/taxonomies.php';
+require_once HAVENSTONE_CORE_PATH.'includes/property-meta.php';
+require_once HAVENSTONE_CORE_PATH.'includes/query.php';
+require_once HAVENSTONE_CORE_PATH.'includes/shortcodes.php';
