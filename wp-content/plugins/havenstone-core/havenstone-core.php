4a1f69376b57f436f2e172cc411e1d9ae447a5aa
kenny6292 <kennymichael322@gmail.com>
2026-09-20 07:36:01 +0100

    Initialize HavenStone Realty WordPress architecture: wp-content/plugins/havenstone-core/havenstone-core.php

---
+++ wp-content/plugins/havenstone-core/havenstone-core.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: HavenStone Realty Core
+ * Description: Core property, agent, enquiry and real-estate functionality for HavenStone Realty.
+ * Version: 1.0.0
+ * Author: DEYOUNGTECH
+ * Requires PHP: 8.1
+ */
+if (!defined('ABSPATH')) exit;
+
+define('HAVENSTONE_CORE_VERSION', '1.0.0');
+define('HAVENSTONE_CORE_PATH', plugin_dir_path(__FILE__));
+
+require_once HAVENSTONE_CORE_PATH . 'includes/post-types.php';
+require_once HAVENSTONE_CORE_PATH . 'includes/taxonomies.php';
