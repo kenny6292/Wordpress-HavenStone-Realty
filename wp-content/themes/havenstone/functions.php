96318df79c25ea3526f6fdf0c55da5e2043b19e1
kenny6292 <kennymichael322@gmail.com>
2026-09-20 07:35:36 +0100

    Initialize HavenStone Realty WordPress architecture: wp-content/themes/havenstone/functions.php

---
+++ wp-content/themes/havenstone/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * HavenStone Realty theme bootstrap.
+ */
+if (!defined('ABSPATH')) exit;
+
+require_once get_template_directory() . '/inc/setup.php';
+require_once get_template_directory() . '/inc/enqueue.php';
+require_once get_template_directory() . '/inc/template-functions.php';
