a337232a18dadda30ede5dbf9cfa4cfa56e655c6
kenny6292 <kennymichael322@gmail.com>
2026-09-20 07:55:43 +0100

    Improve responsive viewport and keyboard accessibility

--- wp-content/themes/havenstone/header.php
+++ wp-content/themes/havenstone/header.php
@@ -2,11 +2,12 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo('charset'); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+<a class="skip-link" href="#main-content">Skip to content</a>
 <header class="site-header">
 <div class="site-container">
 <a class="site-logo" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
