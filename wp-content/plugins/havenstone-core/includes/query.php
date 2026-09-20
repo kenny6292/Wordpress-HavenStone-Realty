6fc228d60dd98ea8dc956838ac95409b338eb746
kenny6292 <kennymichael322@gmail.com>
2026-09-20 07:36:49 +0100

    Build HavenStone property data and search foundation

---
+++ wp-content/plugins/havenstone-core/includes/query.php
@@ -0,0 +1,31 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+function havenstone_property_query(array $args = []): WP_Query {
+    $defaults = [
+        'post_type' => 'property',
+        'post_status' => 'publish',
+        'posts_per_page' => 12,
+        'paged' => max(1, (int) get_query_var('paged')),
+    ];
+    $args = wp_parse_args($args, $defaults);
+
+    $tax_query = [];
+    foreach (['property_type','property_location','property_status'] as $taxonomy) {
+        if (!empty($_GET[$taxonomy])) {
+            $tax_query[] = [
+                'taxonomy' => $taxonomy,
+                'field' => 'slug',
+                'terms' => sanitize_title(wp_unslash($_GET[$taxonomy])),
+            ];
+        }
+    }
+    if ($tax_query) $args['tax_query'] = array_merge(['relation' => 'AND'], $tax_query);
+
+    if (!empty($_GET['min_price']) || !empty($_GET['max_price'])) {
+        $args['meta_query'] = ['relation' => 'AND'];
+        if (!empty($_GET['min_price'])) $args['meta_query'][] = ['key'=>'_havenstone_price','value'=>(float)$_GET['min_price'],'type'=>'NUMERIC','compare'=>'>='];
+        if (!empty($_GET['max_price'])) $args['meta_query'][] = ['key'=>'_havenstone_price','value'=>(float)$_GET['max_price'],'type'=>'NUMERIC','compare'=>'<='];
+    }
+    return new WP_Query($args);
+}
