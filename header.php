22899af25776d38bb3393a85f679969a4427a72e
kenny6292 <kennymichael322@gmail.com>
2026-09-21 09:36:36 +0100

    Refine HavenStone premium header layout

--- header.php
+++ header.php
@@ -8,24 +8,38 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <a class="skip-link" href="#main-content">Skip to content</a>
+
 <header class="site-header">
-<div class="site-container">
-<div class="site-header__bar">
-<a class="site-logo" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-<button class="site-menu-toggle" type="button" aria-expanded="false" aria-controls="primary-menu" aria-label="<?php esc_attr_e('Open navigation menu', 'havenstone'); ?>">
-<span></span><span></span><span></span>
-</button>
-</div>
-<nav id="primary-menu" class="site-nav" aria-label="<?php esc_attr_e('Primary navigation', 'havenstone'); ?>">
-<?php
-wp_nav_menu([
-    'theme_location' => 'primary',
-    'fallback_cb' => 'havenstone_fallback_menu',
-    'container' => false,
-    'menu_id' => 'primary-menu-list',
-]);
-?>
-</nav>
-<a class="site-header__cta button button--small" href="<?php echo esc_url(home_url('/request-a-property/')); ?>">Request a Property</a>
-</div>
+  <div class="site-container site-header__inner">
+    <div class="site-header__bar">
+      <a class="site-logo" href="<?php echo esc_url(home_url('/')); ?>">
+        <?php bloginfo('name'); ?>
+      </a>
+
+      <button
+        class="site-menu-toggle"
+        type="button"
+        aria-expanded="false"
+        aria-controls="primary-menu"
+        aria-label="<?php esc_attr_e('Open navigation menu', 'havenstone'); ?>"
+      >
+        <span></span><span></span><span></span>
+      </button>
+    </div>
+
+    <nav id="primary-menu" class="site-nav" aria-label="<?php esc_attr_e('Primary navigation', 'havenstone'); ?>">
+      <?php
+      wp_nav_menu([
+          'theme_location' => 'primary',
+          'fallback_cb' => 'havenstone_fallback_menu',
+          'container' => false,
+          'menu_id' => 'primary-menu-list',
+      ]);
+      ?>
+    </nav>
+
+    <a class="site-header__cta button button--small" href="<?php echo esc_url(home_url('/request-a-property/')); ?>">
+      Request a Property
+    </a>
+  </div>
 </header>
