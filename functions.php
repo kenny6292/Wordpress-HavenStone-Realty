3910eb3b0a10fc29187b8fecdba476bda0c7a0d9
kenny6292 <kennymichael322@gmail.com>
2026-09-21 08:07:18 +0100

    Load bundled HavenStone Core from installable theme

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+require_once get_template_directory() . '/inc/setup.php';
+require_once get_template_directory() . '/inc/enqueue.php';
+require_once get_template_directory() . '/inc/template-functions.php';
+
+// Bundled HavenStone Core: property, agent, enquiry and viewing-request functionality.
+require_once get_template_directory() . '/havenstone-core/havenstone-core.php';
